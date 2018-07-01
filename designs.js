7250af1b2c65d24dd1d1d56db23cf7ce713f7fae
Mary Waithera <maryapitaz@gmail.com>
2018-07-01 22:46:38 +0300

    Draw grid and add jquery

--- designs.js
+++ designs.js
@@ -3,8 +3,25 @@
 
 // When size is submitted by the user, call makeGrid()
 
+$(document).ready(function() {
+	makeGrid();
+});
+
+
 function makeGrid() {
 
-// Your code goes here!
+let inputHeight = $("#inputHeight").val();
+let inputWidth = $("#inputWidth").val();
+let grid_table = $("#pixelCanvas");
+
+// draw the grid 
+for (let r = 1; r <= inputHeight; r++) {
+    // create row
+    let row = $('<tr></tr>').appendTo(grid_table);
+    for (let c = 1; c <= inputWidth; c++) {
+        // create table cells (columns)
+        row.append('<td></td>');
+    }
+}
 
 }
